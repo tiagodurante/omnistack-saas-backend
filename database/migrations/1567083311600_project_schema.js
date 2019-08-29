5ca3d0a8af9c7bf8beb4b3bb6e3c72474de5039d
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-29 10:35:51 -0300

    migrations configuradas

--- database/migrations/1567083311600_project_schema.js
+++ database/migrations/1567083311600_project_schema.js
@@ -1,19 +1,28 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class ProjectSchema extends Schema {
-  up () {
-    this.create('projects', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("projects", table => {
+      table.increments();
+      table.string("title").notNullable();
+      table
+        .integer("team_id")
+        .unsignetd()
+        .notNullable()
+        .references("id")
+        .inTable("teams")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('projects')
+  down() {
+    this.drop("projects");
   }
 }
 
-module.exports = ProjectSchema
+module.exports = ProjectSchema;
