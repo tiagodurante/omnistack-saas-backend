ff6aa0a2973790f583d5564a25ab8bc2e9ce658c
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-29 11:05:57 -0300

    populando banco e criando a primeira rota

---
+++ database/seeds/DatabaseSeeder.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| DatabaseSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const User = use("App/Models/User");
+
+class DatabaseSeeder {
+  async run() {
+    const user = await User.create({
+      name: "Tiago Amado Durante",
+      email: "tiagodurante@outlook.com.br",
+      password: "123456"
+    });
+    await user.teams().create({
+      name: "Rocketseat",
+      user_id: user.id
+    });
+  }
+}
+
+module.exports = DatabaseSeeder;
