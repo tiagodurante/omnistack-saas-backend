6a35f329b50e0ce602674182a1deac6097c0aac9
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-29 14:32:06 -0300

    hook de convites criado

--- app/Controllers/Http/InviteController.js
+++ app/Controllers/Http/InviteController.js
@@ -1,8 +1,18 @@
 "use strict";
 
+const Invite = use("App/Models/Invite");
+
 class InviteController {
   async store({ request, auth }) {
-    console.log(request.team);
+    const invites = request.input("invites");
+
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }));
+
+    await Invite.createMany(data);
   }
 }
 
