d402e874a3137d394d6bc91fe1fd1da13d980f44
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-30 14:06:33 -0300

    crud de projetos

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -17,19 +17,10 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index({ request }) {
+    const projects = await request.team.projects().fetch();
 
-  /**
-   * Render a form to be used for creating a new project.
-   * GET projects/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return projects;
   }
 
   /**
@@ -40,7 +31,12 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request }) {
+    const data = request.only(["title"]);
+
+    const project = await request.team.projects().create(data);
+
+    return project;
   }
 
   /**
@@ -52,19 +48,13 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, request }) {
+    const project = await request.team
+      .projects()
+      .where("id", params.id)
+      .first();
 
-  /**
-   * Render a form to update an existing project.
-   * GET projects/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return project;
   }
 
   /**
@@ -75,7 +65,17 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request }) {
+    const data = request.only(["title"]);
+    const project = await request.team
+      .projects()
+      .where("id", params.id)
+      .first();
+
+    project.merge(data);
+    await project.save();
+
+    return project;
   }
 
   /**
@@ -86,8 +86,14 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request }) {
+    const project = await request.team
+      .projects()
+      .where("id", params.id)
+      .first();
+
+    await project.delete();
   }
 }
 
-module.exports = ProjectController
+module.exports = ProjectController;
