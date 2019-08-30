6a66b5f4c92ecf3a074804d2dc054b12beda311d
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-30 14:34:51 -0300

    configurando acl

--- app/Models/User.js
+++ app/Models/User.js
@@ -17,6 +17,34 @@ class User extends Model {
     });
   }
 
+  teamJoins() {
+    return this.hasMany("App/Models/UserTeam");
+  }
+
+  async is(expression) {
+    const team = await this.teamJoins()
+      .where("team_id", this.currentTeam)
+      .first();
+
+    return team.is(expression);
+  }
+
+  async can(expression) {
+    const team = await this.teamJoins()
+      .where("team_id", this.currentTeam)
+      .first();
+
+    return team.can(expression);
+  }
+
+  async scope(required) {
+    const team = await this.teamJoins()
+      .where("team_id", this.currentTeam)
+      .first();
+
+    return team.scope(required);
+  }
+
   tokens() {
     return this.hasMany("App/Models/Token");
   }
