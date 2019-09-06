e8f155dec8d150d73404626b4acc8581daf1ce0a
Tiago Amado Durante <tiagodurante@icloud.com>
2019-09-06 10:50:16 -0300

    buscando permissões do usuário

--- start/routes.js
+++ start/routes.js
@@ -43,4 +43,6 @@ Route.group(() => {
   Route.put("members/:id", "MemberController.update").middleware(
     "is:administrator"
   );
+
+  Route.get("permissions", "PermissionController.show");
 }).middleware(["auth", "team"]);
